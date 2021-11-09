f246cd8e64cb9971297dca79c8d1d70475dbbca2
Luuk van Keeken <luukvankeeken24@gmail.com>
2021-11-09 13:52:41 +0100

    Depth data

--- plot_depth_data.py
+++ plot_depth_data.py
@@ -31,21 +31,21 @@ ax = sns.heatmap(a, mask=mask)
 
 # Perform K-means clustering to find the centers of each object.
 #   The found centers are plotted on top of the masked depth image.
-kmeans = KMeans(n_clusters=5, random_state=0).fit(a_correct)
-print(kmeans.cluster_centers_)
-for row in kmeans.cluster_centers_:
-    plt.scatter(x=row[1], y=row[0]) # x and y are switched because the created cluster centers are otherwise mirrored in line y=-x.
+# kmeans = KMeans(n_clusters=5, random_state=0).fit(a_correct)
+# print(kmeans.cluster_centers_)
+# for row in kmeans.cluster_centers_:
+#     plt.scatter(x=row[1], y=row[0]) # x and y are switched because the created cluster centers are otherwise mirrored in line y=-x.
 
 
 # Perform spectral clustering to find the five objects. 
-# spectral = SpectralClustering(n_clusters=5, ).fit(a_correct)
-# print(spectral.labels_)
-# a_x = []
-# a_y = []
-# for point in a_correct:
-#     a_x.append(point[0])
-#     a_y.append(point[1])
-# plt.scatter(a_y, a_x, c=spectral.labels_) # a_y and a_x are switched because the created clustered image is otherwise mirrored in line y=-x.
+spectral = SpectralClustering(n_clusters=5, ).fit(a_correct)
+print(spectral.labels_)
+a_x = []
+a_y = []
+for point in a_correct:
+    a_x.append(point[0])
+    a_y.append(point[1])
+plt.scatter(a_y, a_x, c=spectral.labels_) # a_y and a_x are switched because the created clustered image is otherwise mirrored in line y=-x.
 
 
 plt.show()
