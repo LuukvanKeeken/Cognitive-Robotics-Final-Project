96043f476829d6aec18951eb8d3ce58639a3f5ce
kvanschuijlenburg <kvanschuijlenburg@gmail.com>
2021-11-17 17:58:35 +0100

    commit on 'new_model'

--- summarizeResults.py
+++ summarizeResults.py
@@ -1,3 +1,4 @@
+import math
 import os
 import pandas
 import numpy as np
@@ -6,6 +7,30 @@ import matplotlib.pyplot as plt
 
 directory = "./results"
 tableData = []
+
+def normalApproximation(sample):
+    sampleSize = len(sample)
+    p = sample.mean()
+    q = 1-p
+
+    if not (sampleSize * p > 5 and sampleSize * q > 5):
+        return -1,-1
+    mu = p * sampleSize
+    std = math.sqrt(mu*q)
+  
+    return mu, std
+
+def normalApproximationFromMean(sampleSize, p):
+    q = 1-p
+
+    if not (sampleSize * p > 5 and sampleSize * q > 5):
+        return -1,-1
+    mu = p * sampleSize
+    std = math.sqrt(mu*q)
+  
+    return mu, std
+
+
 for filename in os.listdir(directory):
     if filename.endswith(".csv"):
         experiment = filename.replace(',', '')
@@ -23,14 +48,24 @@ for filename in os.listdir(directory):
         runs = len(df.index)
 
         runsSuccess = df.correctObjectManipulations.mean()
-        predictionSuccess = df.correctObjectMatch.sum() / df.totalPredictions.sum()
+        meanRunsSuccess, STDrunsSuccess = normalApproximation(df.correctObjectManipulations)
+        #runsSuccessSTD = df.correctObjectManipulations.std()
+        
+        predictionSuccesDividedRun = df.correctObjectMatch / df.totalPredictions
+        meanPredictionSuccessPerRun = predictionSuccesDividedRun.mean()
+        STDPredictionSuccessPerRun = predictionSuccesDividedRun.std()
+        predictionSuccesPerRun = df.correctObjectMatch.sum() / df.totalPredictions.sum()
         
         runsWithoutPileManipulation = len(df[df.pileManipulations == 0])/runs
-        averagePileManipulationsPerRun = df.pileManipulations.mean()
+        meanRunsWithoutPileManipulation,STDRunsWithoutPileManipulation = normalApproximationFromMean(runs, runsWithoutPileManipulation)
 
-        atleastOneCorrectPredictionPerRun = len(df[df.correctObjectMatch>0])/runs
+        meanPileManipulations = df.pileManipulations.mean()
+        STDPileManipulations= df.pileManipulations.std()
+
+        runsWithCorrectPredictionPerRun = len(df[df.correctObjectMatch>0])/runs
+        meanRunsWithCorrectPredictionPerRun,STDRunsWithCorrectPredictionPerRun = normalApproximationFromMean(runs, runsWithCorrectPredictionPerRun)
         
-        tableRow = [scenario, segmenter, matchingNetwork, graspingNetwork, runsSuccess, predictionSuccess, runsWithoutPileManipulation, averagePileManipulationsPerRun, atleastOneCorrectPredictionPerRun]
+        tableRow = [scenario, segmenter, matchingNetwork, graspingNetwork, meanRunsSuccess, STDrunsSuccess, predictionSuccesPerRun, meanPredictionSuccessPerRun, STDPredictionSuccessPerRun, meanRunsWithoutPileManipulation,STDRunsWithoutPileManipulation, meanPileManipulations,STDPileManipulations, meanRunsWithCorrectPredictionPerRun,STDRunsWithCorrectPredictionPerRun]
         tableData.append(tableRow)
    
 
@@ -45,6 +80,9 @@ for filename in os.listdir(directory):
 
     else:
         continue
+columns = ['scenario', 'segmenter', 'matchingNetwork', 'graspingNetwork', 'meanRunSuccess', 'STDRunSuccess', 'predictionSuccesPerRun', 'meanPredictionSuccessPerRun', 'STDPredictionSuccessPerRun ', 'meanRunsWithoutPileManipulation', 'STDRunsWithoutPileManipulation', 'meanPileManipulations', 'STDPileManipulations', 'meanRunsWithCorrectPredictionPerRun', 'STDRunsWithCorrectPredictionPerRun']
 
-table = pandas.DataFrame(np.array(tableData), columns = ['scenario', 'segmenter', 'matchingNetwork', 'graspingNetwork', 'runsSuccess', 'predictionSuccess', 'runsWithoutPileManipulation','averagePileManipulationsPerRun', 'atleastOneCorrectPredictionPerRun'])
+
+table = pandas.DataFrame(np.array(tableData), columns = columns)
 print(table.to_markdown())
+table.to_csv("summary.csv")
